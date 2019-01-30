dd8825aab913872478c0f7c43e61f826ce6d16a5
AlyonaTiki <31675522+AlyonaTiki@users.noreply.github.com>
2019-01-30 12:22:12 -0500

    Initial commit

---
+++ lab1_flower.py
@@ -0,0 +1,12 @@
+#Alyona Karmazin
+#1/30/19
+#this program draws flower
+
+import turtle
+tim = turtle.Turtle()
+for i in range(45):
+    tim.forward(100)
+    tim.right(92)
+    tim.forward(10)
+    tim.right(92)
+
---
+++ lab1_moto.py
@@ -0,0 +1,7 @@
+#Alyona Karmazin
+#1/30/19
+#this program prints motto 19 times
+
+for i in range(19):
+   print("Mihi cura futuri")
+
